0e345fed3bf9bf5e42282847cd19cb7b0aaedb61
Francisco Arley Coelho Barros <107206805+Ch1c40@users.noreply.github.com>
2023-04-13 16:43:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if valor == 2:
     a = float(input("Digite o valor de a: "))
     if a == 0:
         print("Valor de a inválido")
-    elif a != 0:
+    elif a < 0:
         b = float(input("Digite o valor de b: "))
         c = float(input("Digite o valor de c: "))
         calculo2 = b**2 - 4 * a * c
